2bf81528d19ecf4efc2dd4eb090975c977d8f0f7
denis909 <denis909@mail.ru>
2018-11-19 15:29:16 +0300

    n/a

---
+++ backend/grid/GridView.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace backend\grid;
+
+class GridView extends \denis909\yii\grid\GridView
+{
+}
---
+++ frontend/grid/GridView.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace frontend\grid;
+
+class GridView extends \denis909\yii\grid\GridView
+{
+}
